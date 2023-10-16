9e0b5961552edf8e57a4ca6ab4b36abdcaa7229e
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2023-10-16 07:37:35 +0100

    logo reroute

--- src/components/layouts/NavBar/index.tsx
+++ src/components/layouts/NavBar/index.tsx
@@ -2,18 +2,24 @@ import Image from 'next/image';
 import React from 'react'
 import NavBarItem from './NavBarItem';
 import { NavBarContents } from '@/utils/constants';
+import { useRouter } from 'next/router';
 
 function NavBar() {
+    const router = useRouter();
   return (
     <div className='w-[15vw] md:w-[20vw] h-full border-3 border-r bg-white px-2 py-10 md:p-10 fixed z-[20]'>
-        <div className='flex flex-row items-center cursor-pointer mb-20'>
+        <div className='flex flex-row items-center mb-20'
+        >
             <Image
                 src={require("../../../../public/svg/logo.svg")}
                 alt="SIRP Logo"
                 width={50}
                 height={50}
-                className='md:mr-[20px]'
+                className='md:mr-[20px] cursor-pointer'
                 priority
+                onClick={() => {
+                    router.push("http://192.81.213.226:30");
+                  }}
             />
             <h1 className='text-sirp-primary font-semibold text-[22px] hidden md:block'>Fact Checker</h1>
         </div>
