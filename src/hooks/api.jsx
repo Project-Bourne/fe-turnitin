61f5ab1136ae8618cb1066f9a03d0c3db37b684f
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2023-10-24 22:43:58 +0100

    fix error

--- src/hooks/api.jsx
+++ src/hooks/api.jsx
@@ -36,7 +36,7 @@ export async function request(url, method, payload, token, text, form) {
       .then(res => {
         if (res.status === 403) {
           // Redirect to the login page
-          // window.location.href = 'http://192.81.213.226:30/auth/login';
+          window.location.href = 'http://192.81.213.226:30/auth/login';
           throw new Error('Access forbidden. Redirecting to login page.');
         }
         else if (text === true) {
@@ -88,7 +88,7 @@ export async function request2(url, method, payload, token, text, form) {
       .then(res => {
         if (res.status === 403) {
           // Redirect to the login page
-          // window.location.href = 'http://192.81.213.226:30/auth/login';
+          window.location.href = 'http://192.81.213.226:30/auth/login';
           throw new Error('Access forbidden. Redirecting to login page.');
         }
         else if (text === true) {
